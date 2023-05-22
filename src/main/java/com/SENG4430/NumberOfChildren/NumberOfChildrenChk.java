b7d800163b40c6b65ef4021c1e9cf3afaf8a3656
Simon28-G <you@example.com>
2023-05-22 14:32:38 +1000

    fix: NOC tests were gone

--- src/main/java/com/SENG4430/NumberOfChildren/NumberOfChildrenChk.java
+++ src/main/java/com/SENG4430/NumberOfChildren/NumberOfChildrenChk.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import spoon.Launcher;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.Query;
 import spoon.reflect.visitor.filter.TypeFilter;
 
@@ -26,12 +28,34 @@ public class NumberOfChildrenChk {
     public void check(Launcher launcher) {
         // Loop through all the classes in the code using Spoon framework
         for (CtClass<?> classObject : Query.getElements(launcher.getFactory(), new TypeFilter<>(CtClass.class))) {
-
-            int numberOfChildren = classObject.getNestedTypes().size();
-            numberOfChildrenChk
-                    .add(new java.util.AbstractMap.SimpleEntry<>(classObject.getSimpleName(), numberOfChildren));
+            if (!classObject.isInterface()) {
+                int numberOfChildren = calculateNumberOfChildren(classObject);
+                numberOfChildrenChk
+                        .add(new java.util.AbstractMap.SimpleEntry<>(classObject.getSimpleName(), numberOfChildren));
+            }
         }
 
+    }
+
+    /**
+     * Calculates the number of direct children for a given class.
+     *
+     * @param classObject The CtClass representing the class to calculate the number
+     *                    of children for.
+     * @return The number of direct children for the class.
+     */
+    private int calculateNumberOfChildren(CtClass<?> classObject) {
+        int numberOfChildren = 0;
+        for (CtElement element : classObject.getPackage().getElements(new TypeFilter<>(CtClass.class))) {
+
+            CtTypeReference<?> superClass = ((CtClass<?>) element).getSuperclass();
+
+            // Check if the element has a superclass and its superclass is the current class
+            if (superClass != null && superClass.getQualifiedName().equals(classObject.getQualifiedName())) {
+                numberOfChildren++;
+            }
+        }
+        return numberOfChildren;
     }
 
 }
